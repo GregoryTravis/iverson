9b8ac19c76ce44f6b889a5cd3a01173cffbb4834
Greg Travis <gmt@dhcp-172-26-104-196.nyc.corp.google.com>
2010-11-05 12:35:08 -0400

    showing a table

--- iverson.js
+++ iverson.js
@@ -1,29 +1,29 @@
-function shew( s )
+function shew(s)
 {
-  var e = document.createTextNode( s+"" );
-  var body = document.getElementsByTagName( "body" )[0];
-  body.appendChild( e );
-  body.appendChild( document.createElement( "br" ) );
+  var e = document.createTextNode(s+"");
+  var body = document.getElementsByTagName("body")[0];
+  body.appendChild(e);
+  body.appendChild(document.createElement("br"));
 }
 
-window.onerror = function( a, b, c )
+window.onerror = function(a, b, c)
 {
   try {
     var message =  "Exception: "+a+" "+b+" "+c;
-    shew( message );
-  } catch( e ) {
+    shew(message);
+  } catch(e) {
     var s = "";
     if (e["type"]) { s += e["type"]+"; "; }
     if (e["message"]) { s += e["message"]+"; "; }
     if (e["description"]) { s += e["description"]+"; "; }
     if (e["number"]) { s += e["number"]+"; "; }
-    alert( s );
+    alert(s);
     throw e;
   }
   return true;
 };
 
-function include( file )
+function include(file)
 {
   file = "file:///Users/gmt/.../lib/" + file;
   if (__include.files[file]) {
@@ -35,14 +35,43 @@ function include( file )
     file += "?s="+new Date().getTime();
   }
 
-  var script = document.createElement( "script" );
+  var script = document.createElement("script");
   script.src = file;
-  var head = document.getElementsByTagName( "head" )[0];
-  head.appendChild( script );
+  var head = document.getElementsByTagName("head")[0];
+  head.appendChild(script);
 
   if (__include.verbose) {
-    shew( "include "+file );
+    shew("include "+file);
   }
 }
 
-shew(34);
+function elem(type)
+{
+  return document.createElement( type );
+}
+
+function maketable(rows) {
+  var table = elem("table");
+  table.border = 1;
+  if (rows.length == 0) {
+    return maketable([{nothing: "nothing"}]);
+  }
+  var keyset = Object.keys(rows[0]);
+  for (var i = 0; i < rows.length; ++i) {
+    var row = rows[i];
+    var tr = elem("tr");
+    for (iii in keyset) {
+      var key = keyset[iii];
+      var value = row[key];
+      var td = elem("td");
+      td.appendChild(document.createTextNode(value + ""));
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
+  var body = document.getElementsByTagName("body")[0];
+  body.appendChild(table);
+}
+
+maketable([{a: 10, b: 20}, {a: 100, b: 200}]);
+
