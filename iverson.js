68713e0791b1fbfda5f2d1b81fb9bbe13668cd25
Greg Travis <gmt@dhcp-172-26-104-196.nyc.corp.google.com>
2010-11-05 11:59:55 -0400

    shewing

--- iverson.js
+++ iverson.js
@@ -1,1 +1,48 @@
-alert(1);
+function shew( s )
+{
+  var e = document.createTextNode( s+"" );
+  var body = document.getElementsByTagName( "body" )[0];
+  body.appendChild( e );
+  body.appendChild( document.createElement( "br" ) );
+}
+
+window.onerror = function( a, b, c )
+{
+  try {
+    var message =  "Exception: "+a+" "+b+" "+c;
+    shew( message );
+  } catch( e ) {
+    var s = "";
+    if (e["type"]) { s += e["type"]+"; "; }
+    if (e["message"]) { s += e["message"]+"; "; }
+    if (e["description"]) { s += e["description"]+"; "; }
+    if (e["number"]) { s += e["number"]+"; "; }
+    alert( s );
+    throw e;
+  }
+  return true;
+};
+
+function include( file )
+{
+  file = "file:///Users/gmt/.../lib/" + file;
+  if (__include.files[file]) {
+    return;
+  }
+  __include.files[file] = 1;
+
+  if (__include.alwaysReload) {
+    file += "?s="+new Date().getTime();
+  }
+
+  var script = document.createElement( "script" );
+  script.src = file;
+  var head = document.getElementsByTagName( "head" )[0];
+  head.appendChild( script );
+
+  if (__include.verbose) {
+    shew( "include "+file );
+  }
+}
+
+shew(34);
