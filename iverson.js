2c760995bec7e9a9222e81de6c58de8aafa6591d
Greg Travis <gmt@dhcp-172-26-104-174.nyc.corp.google.com>
2010-11-10 11:07:35 -0500

    Crude, messy default table.

--- iverson.js
+++ iverson.js
@@ -1,6 +1,10 @@
+function text(o) {
+  return document.createTextNode(o + "");
+}
+
 function shew(s)
 {
-  var e = document.createTextNode(s+"");
+  var e = text(s);
   var body = document.getElementsByTagName("body")[0];
   body.appendChild(e);
   body.appendChild(document.createElement("br"));
@@ -85,6 +89,72 @@ function maketable(rows) {
   return table;
 }
 
+function keys(o) {
+  var arr = new Array();
+  var inx = 0;
+  for (k in o) {
+    arr[inx++] = k;
+  }
+  return arr;
+}
+
+function values(o) {
+  var arr = new Array();
+  var inx = 0;
+  for (k in o) {
+    arr[inx++] = o[k];
+  }
+  return arr;
+}
+
+function horizontalArraystable(arrays) {
+  var table = elem("table");
+  table.border = 1;
+  for (var i = 0; i < arrays.length; ++i) {
+    var tr = elem("tr");
+    var row = arrays[i];
+    for (var j = 0; j < row.length; ++j) {
+      var td = elem("td");
+      var value = row[j];
+      value = (value instanceof Node) ? value : text(value);
+      td.appendChild(value);
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
+  return table;
+}
+
+function verticalArrayTable(array) {
+  var table = elem("table");
+  table.border = 1;
+  for (var i = 0; i < array.length; ++i) {
+    var tr = elem("tr");
+    var td = elem("td");
+    var value = array[i];
+    value = (value instanceof Node) ? value : text(value);
+    td.appendChild(value);
+    tr.appendChild(td);
+    table.appendChild(tr);
+  }
+  return table;
+}
+
+function dumtable(o) {
+  if (isScalar(o)) {
+    return "" + o;
+  } else if (isList(o)) {
+    var vals = values(o);
+    vals = vals.map(function (value) { return dumtable(value); });
+    return verticalArrayTable(vals);
+  } else if (isRecord(o)) {
+    var ks = keys(o);text
+    var vals = values(o);
+    vals = vals.map(function (value) { return dumtable(value); });
+    return horizontalArraystable([ks, vals]);
+  }
+}
+
 function isScalar(o) {
   return !(o instanceof Object) && !(o instanceof Array);
 }
@@ -97,13 +167,14 @@ function isList(o) {
   return o instanceof Array;
 }
 
-shew(3 instanceof Array);
-shew([3] instanceof Array);
-shew({a:3} instanceof Array);
-shew(3 instanceof Object);
-shew([3] instanceof Object);
-shew({a:3} instanceof Object);
+/* shew(3 instanceof Array); */
+/* shew([3] instanceof Array); */
+/* shew({a:3} instanceof Array); */
+/* shew(3 instanceof Object); */
+/* shew([3] instanceof Object); */
+/* shew({a:3} instanceof Object); */
 
-put(maketable([{a: 10, b: 20}, {a: 100, b: 200}]));
-
-put(maketable(jeter));
+//put(maketable([{a: 10, b: 20}, {a: 100, b: 200}]));
+//put(maketable(jeter));
+put(dumtable([{a: 10, b: 20}, {a: 100, b: 200}]));
+put(dumtable(jeter));
